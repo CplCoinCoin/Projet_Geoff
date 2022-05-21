74b13221eb6192b8a5ca9fb83da0409b32ad033b
CplCoinCoin <RMouhibi@outlook.fr>
2022-05-21 20:34:07 +0200

    Test

---
+++ Projet/src/Controller/TestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller ;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class TestController extends AbstractController 
+{
+
+
+        /**
+     * @Route("/test", name="indextest")
+     */
+    public function index ()
+    {
+        var_dump("Hello");
+        die();
+    }
+
+
+}
+
+
+?>
